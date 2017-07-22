00917b74b93bc8816867165ac6716390be23df81
wweihs <wendyweihs@gmail.com>
2017-07-22 09:04:53 -0500

    Adding git pull

--- github.php
+++ github.php
@@ -4,8 +4,6 @@ require_once('Git.php');
 
 $repo = Git::open('~/public_html/wendydev/drupal8/TravisIntegration');  // -or- Git::create('/path/to/repo')
 
-$repo->add('.');
-$repo->commit('Some commit message');
-$repo->push('origin', 'master');
+$repo->pull('origin', 'master');
 
 ?>
